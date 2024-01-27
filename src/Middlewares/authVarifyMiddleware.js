918ccc9d5372d41beb44abd73baee68a8e543012
Naim Hossain <naimh200@gmail.com>
2024-01-27 15:10:34 +0600

    set middlewares

--- src/Middlewares/authVarifyMiddleware.js
+++ src/Middlewares/authVarifyMiddleware.js
@@ -1,9 +1,33 @@
 import jwt from 'jsonwebtoken'
+import 'dotenv/config'
 import extractToken from "../utilitis/extractToken.js";
-
-export const adminAuthVerification = async (req,res,next,ad)=>{
+import UserSchema from '../Model/userModel.js'
+export const signInRequired = async (req,res,next)=>{
     const token=await extractToken(req.header('Authorization')) //split bearer from Authorization token
-    console.log(ad)
+    try{
+        if(!token || token===null){
+            return res.status(401).json({status: "Unauthorized Action"})
+        }
+        else{
+            const decoded=jwt.verify(token,process.env.PrivateKey)
+            const user=await UserSchema.findById(decoded._id,{'password':0})
+            console.log(user)
+            req.user=user
+            next()
+        }
+    }
+    catch (err) {
+        return res.status(501).json({status:"token has been expired" ,message:err})
+    }
+
+}
+export const allowTo = (...roles)=>{
+    return async (req,res,next)=>{
+        if (!roles.includes(req.user.role)){
+            return res.status(401).json({status:'Failed! You are Not allow to this route' })
+        }
+        next()
+    }
 }
 
 
--- src/Routes/featureRoute.js
+++ src/Routes/featureRoute.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import {CreateFeature} from "../Controllers/featureController.js";
-import {adminAuthVerification} from "../Middlewares/authVarifyMiddleware.js";
+import {signInRequired,allowTo} from "../Middlewares/authVarifyMiddleware.js";
 const router= express.Router()
 
-router.get("/",adminAuthVerification,(req,res)=>{
+router.get("/",signInRequired,allowTo('student','teacher'),(req,res)=>{
     res.send("Work")
 })
 router.post("/create",CreateFeature)
